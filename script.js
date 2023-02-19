7f779b39228e5f5f9134e2a01e3cef9ea2cb66f2
Samrat Ghosh <samratghosh490@gmail.com>
2023-02-19 10:18:53 +0530

    attach HTML, CSS, JS files

---
+++ script.js
@@ -0,0 +1,27 @@
+var cel = document.getElementById("cel");
+var fah = document.getElementById("fah");
+var kel = document.getElementById("kel");
+
+cel.addEventListener('input', function() {
+    let c = this.value;
+    let f = (c * 9) / 5 + 32;
+    let k = parseFloat(c) + 273.15; // convert c to float and add 273.15
+    kel.value = k;
+    fah.value = f.toFixed(3);
+});
+
+fah.addEventListener('input', function() {
+    let f = this.value;
+    let c = (f - 32) * 5 / 9;
+    let k = (f + 459.67) * 5 / 9;
+    cel.value = c;
+    kel.value = k;
+});
+
+kel.addEventListener('input', function() {
+    let k = this.value;
+    let c = k - 273.15;
+    let f = c * 9 / 5 + 32;
+    fah.value = f.toFixed(3);
+    cel.value = c;
+});
