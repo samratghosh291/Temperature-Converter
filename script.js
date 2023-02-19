3ef54f0ed79c9a3be1df59948d82191cfda947f6
Samrat Ghosh <samratghosh490@gmail.com>
2023-02-19 10:28:56 +0530

    js modefied

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ cel.addEventListener('input', function() {
     let c = this.value;
     let f = (c * 9) / 5 + 32;
     let k = parseFloat(c) + 273.15; // convert c to float and add 273.15
-    kel.value = k;
+    kel.value = k.toFixed(3);
     fah.value = f.toFixed(3);
 });
 
@@ -14,8 +14,8 @@ fah.addEventListener('input', function() {
     let f = this.value;
     let c = (f - 32) * 5 / 9;
     let k = (f + 459.67) * 5 / 9;
-    cel.value = c;
-    kel.value = k;
+    cel.value = c.toFixed(3);
+    kel.value = k.toFixed(3);
 });
 
 kel.addEventListener('input', function() {
@@ -23,5 +23,5 @@ kel.addEventListener('input', function() {
     let c = k - 273.15;
     let f = c * 9 / 5 + 32;
     fah.value = f.toFixed(3);
-    cel.value = c;
+    cel.value = c.toFixed(3);
 });
